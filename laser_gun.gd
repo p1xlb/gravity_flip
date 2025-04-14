38fc7883a779a4d499b4d9f11120e77396bb08e3
p1xlb <tempakkul@gmail.com>
2025-04-14 08:31:02 +0800

    hawk 3ah

--- laser_gun.gd
+++ laser_gun.gd
@@ -2,7 +2,8 @@ extends StaticBody2D
 
 @export var laserDirection : String = ""
 
-var laser_scene = preload("res://laser.tscn")
+var laser_scene_horizontal = preload("res://laser.tscn")
+var laser_scene_vertical = preload("res://laser_vertical.tscn")
 var shoot_timer = 0.0
 var shoot_interval = 1.5  # Shoot every 2.5 seconds
 
@@ -16,7 +17,15 @@ func _process(delta):
 		shoot_laser()
 
 func shoot_laser():
-	var laser = laser_scene.instantiate()
+	var laser
+	if laserDirection == "LEFT" or laserDirection == "RIGHT":
+		laser = laser_scene_horizontal.instantiate()
+	elif laserDirection == "UP" or laserDirection == "DOWN":
+		laser = laser_scene_vertical.instantiate()
+	else:
+		print("Invalid laser direction: " + laserDirection)
+		return
+		
 	get_parent().add_child(laser)
 	laser.global_position = $LaserSpawnPoint.global_position
 	if laserDirection == "LEFT":  # Adjust this based on gun rotation
